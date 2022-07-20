3a217518ee4a66eee82efa6d6b0eddc9e4b1b852
ValentinLagues <valentin.lagues@gmail.com>
2022-07-21 00:49:29 +0200

    input linked to context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
 import './App.css';
 
-import React from 'react';
+import React, { useContext } from 'react';
+import { HashRouter as Router, Link, Route, Routes } from 'react-router-dom';
+
+import Playground from './components/playground/Playground';
+import CurrentPlaygroundContext from './contexts/CurrentPlayground';
 
 const App = () => {
+  const {
+    setRowsNumber,
+    rowsNumber,
+    setColumnsNumber,
+    columnsNumber,
+    setHooverLocationX,
+    hooverLocationX,
+    setHooverLocationY,
+    hooverLocationY,
+    setHooverDirection,
+    hooverDirection,
+  } = useContext(CurrentPlaygroundContext);
+  // Fonction qui permet de choisir le nombre de lignes du playground
+  const handleChangeRowsNumber = (e) => {
+    setRowsNumber(e.target.value);
+    console.log(rowsNumber);
+  };
+  // Fonction qui permet de choisir le nombre de colonnes du playground
+  const handleChangeColumnsNumber = (e) => {
+    setColumnsNumber(e.target.value);
+    console.log(columnsNumber);
+  };
+  // Fonction qui permet de choisir la position X de départ du hoover
+  const handleChangeHooverLocationX = (e) => {
+    setHooverLocationX(e.target.value);
+    console.log(hooverLocationX);
+  };
+  // Fonction qui permet de choisir la position Y de départ du hoover
+  const handleChangeHooverLocationY = (e) => {
+    setHooverLocationY(e.target.value);
+    console.log(hooverLocationY);
+  };
+  // Fonction qui permet de choisir l'orientation de départ du hoover
+  const handleChangeHooverDirection = (e) => {
+    setHooverDirection(e.target.value);
+    console.log(hooverDirection);
+  };
+  const onSubmit = () => {
+    <Routes>
+      <Route path="/playground" element={<Playground />} />
+    </Routes>;
+  };
   return (
     <div className="App">
       <h1>Welcome to IHoover playground</h1>
@@ -10,20 +56,34 @@ const App = () => {
         <div>
           <p>Please select the size of the room you want to clean</p>
           <label htmlFor="horizontal">Horizontal</label>
-          <input type="number" min="1" max="100"></input>
+          <input
+            type="number"
+            value={rowsNumber}
+            onChange={handleChangeRowsNumber}></input>
           <label htmlFor="vertical">Vertical</label>
-          <input type="number" min="1" max="100"></input>
+          <input
+            type="number"
+            value={columnsNumber}
+            onChange={handleChangeColumnsNumber}></input>
         </div>
         <div>
           <p>Please select the initial position of your wonderful hoover</p>
           <label htmlFor="horizontal">Horizontal</label>
-          <input type="number" min="1" max="100"></input>
+          <input
+            type="number"
+            value={hooverLocationX}
+            onChange={handleChangeHooverLocationX}></input>
           <label htmlFor="vertical">Vertical</label>
-          <input type="number" min="1" max="100"></input>
+          <input
+            type="number"
+            value={hooverLocationY}
+            onChange={handleChangeHooverLocationY}></input>
           <label htmlFor="direction">Direction</label>
           <select name="directions" id="direction-select">
             <option value="">--Please choose a direction--</option>
-            <option value="N">N</option>
+            <option value="N" onChange={handleChangeHooverDirection}>
+              N
+            </option>
             <option value="E">E</option>
             <option value="W">W</option>
             <option value="S">S</option>
--- src/contexts/CurrentPlayground.tsx
+++ src/contexts/CurrentPlayground.tsx
@@ -5,22 +5,37 @@ type PlaygroundContent = {
   setRowsNumber: React.Dispatch<React.SetStateAction<number>>;
   columnsNumber: number;
   setColumnsNumber: React.Dispatch<React.SetStateAction<number>>;
+  hooverLocationX: number;
+  setHooverLocationX: React.Dispatch<React.SetStateAction<number>>;
+  hooverLocationY: number;
+  setHooverLocationY: React.Dispatch<React.SetStateAction<number>>;
+  hooverDirection: string;
+  setHooverDirection: React.Dispatch<React.SetStateAction<string>>;
 };
 
 type CurrentPlaygroundProps = { children: React.ReactNode };
 
 const CurrentPlaygroundContext = createContext<PlaygroundContent>({
-  rowsNumber: 0,
+  rowsNumber: 1,
   setRowsNumber: () => {},
-  columnsNumber: 0,
+  columnsNumber: 1,
   setColumnsNumber: () => {},
+  hooverLocationX: 0,
+  setHooverLocationX: () => {},
+  hooverLocationY: 0,
+  setHooverLocationY: () => {},
+  hooverDirection: 'N',
+  setHooverDirection: () => {},
 });
 
 export const CurrentPlaygroundContextProvider = ({
   children,
 }: CurrentPlaygroundProps) => {
-  const [rowsNumber, setRowsNumber] = useState<number>(0);
-  const [columnsNumber, setColumnsNumber] = useState<number>(0);
+  const [rowsNumber, setRowsNumber] = useState<number>(1);
+  const [columnsNumber, setColumnsNumber] = useState<number>(1);
+  const [hooverLocationX, setHooverLocationX] = useState<number>(0);
+  const [hooverLocationY, setHooverLocationY] = useState<number>(0);
+  const [hooverDirection, setHooverDirection] = useState<string>('N');
 
   return (
     <CurrentPlaygroundContext.Provider
@@ -29,6 +44,12 @@ export const CurrentPlaygroundContextProvider = ({
         setRowsNumber,
         columnsNumber,
         setColumnsNumber,
+        hooverLocationX,
+        setHooverLocationX,
+        hooverLocationY,
+        setHooverLocationY,
+        hooverDirection,
+        setHooverDirection,
       }}>
       {children}
     </CurrentPlaygroundContext.Provider>
